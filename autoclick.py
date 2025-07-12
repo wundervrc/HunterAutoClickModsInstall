d4b7db9e0d24e0789f579953225da9b44157cf80
wundervrc <wunderburst@gmail.com>
2025-07-12 08:22:32 -0230

    Bug fix

--- autoclick.py
+++ autoclick.py
@@ -7,12 +7,14 @@ import PIL
 
 #settings for hunter:
 confidence = 0.7 #Change this to change the confidence required for the search to be successful.
-
+timeToWaitBetweenChecks = 1 #1 second, can be fractional. like 0.5  lower values will use more cpu due to more frequent checks.
 #Enjoy :3
+
+
 done = False
-input("Hold Q to quit when done :3 ~wunder <3")
+print("Hold Q to quit when done :3 ~wunder <3")
 while not done: #Loop until user input every 1 second
-    time.sleep(1) #This does wait every 1 second, it could possibly run even quicker to shave off time between button presses when they occur but I didn't want to spike cpu usage too much lol. Perhaps it can be changed assuming the library we are using for image recognition is not too heavy.
+    time.sleep(timeToWaitBetweenChecks) #This does wait every 1 second, it could possibly run even quicker to shave off time between button presses when they occur but I didn't want to spike cpu usage too much lol. Perhaps it can be changed assuming the library we are using for image recognition is not too heavy.
     if(keyboard.is_pressed("Q") or keyboard.is_pressed("q")):
         done = True #We are done
         break
@@ -22,15 +24,13 @@ while not done: #Loop until user input every 1 second
     screenshot_np = cv2.cvtColor(screenshot_np, cv2.COLOR_RGB2BGR)
 
     button_image = cv2.imread("button_image.png") #Should be local directory.
-    result = cv2.matchTemplate(screenshot,button_image,cv2.TM_CCOEFF_NORMED)
+    result = cv2.matchTemplate(screenshot_np,button_image,cv2.TM_CCOEFF_NORMED)
     min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
     if max_val >= confidence:
         h,w,_ = button_image.shape
         center_x = max_loc[0] + w // 2 # I assume the max_loc is the left/right most and top/bottom most. Which is why the site recommends adding the length of the image divided by 2 thus giving center.
         center_y = max_loc[1] + h // 2
-        #Perform click action
-        #time.sleep(0.1) #Remove the hashtag on the left side of the word time if this is firing too quickly, it will make it wait however many of a second you enter.
-        pyautogui.leftClick(center_x,center_y) #it will click instantly ideally, if this is too quick we can make it take a small bit of time.
+        pyautogui.leftClick(center_x,center_y)
         print("image found and clicked, Hold Q to quit when done")
 
 print("All done! c:")
